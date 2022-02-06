8f28c132d610f19ea8fec36ac58351baee60f6c4
William Wall <william.a.wall.4@gmail.com>
2022-02-06 08:15:18 -0700

    minor data script improvements
    Consolidate docker command, use `realpath`, and avoid changing directories

--- scripts/data.sh
+++ scripts/data.sh
@@ -14,26 +14,16 @@ COMMAND Options
 EOF
 }
 
-function absolute_path_of_file() {
-	local SOURCE=$0
-  local CWD DIR
-	CWD=$(pwd)
-	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-		DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-		SOURCE=$(readlink "$SOURCE")
-		[[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-	done
-	DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
-	cd "$CWD"
-	echo "$DIR"
+function docker_postgres() {
+  docker run -it --rm --net host --env-file "${ENV_FILE}" -v "${PROJECT_DIR}":/workspace -w /workspace supabase/postgres:14.1.0 "$@"
 }
 
 function load_data() {
-  docker run -it --rm --net host --env-file "${ENV_FILE}" -v "${PROJECT_DIR}":/workspace -w /workspace supabase/postgres:14.1.0 psql -f supabase/seed_data/data.sql
+  docker_postgres psql -f supabase/seed_data/data.sql
 }
 
 function dump_data() {
-  docker run -it --rm --net host --env-file "${ENV_FILE}" -v "${PROJECT_DIR}":/workspace -w /workspace supabase/postgres:14.1.0 \
+  docker_postgres \
     pg_dump --data-only \
     --table public.objectives \
     --table public.games \
@@ -49,14 +39,10 @@ function dump_data() {
     --table auth.identities \
     --table auth.users \
   > supabase/seed_data/data.sql
-
 }
 
-START_DIR=$(pwd)
-SCRIPTS_DIR=$(absolute_path_of_file "${BASH_SOURCE[0]}")
-cd "$SCRIPTS_DIR"
-cd ..
-PROJECT_DIR=$(pwd)
+SCRIPTS_DIR=$(dirname $0)
+PROJECT_DIR=$(realpath "$SCRIPTS_DIR/../")
 ENV_FILE="${PROJECT_DIR}/.env"
 
 # Check for prerequistes
@@ -74,6 +60,3 @@ elif [ "dump" = "$COMMAND" ] ; then
 else
   usage
 fi
-
-
-cd "$START_DIR"
