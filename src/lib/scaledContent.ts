02930248b10684c6d7d2ea7faaa0262a647b2803
William Wall <william.a.wall.4@gmail.com>
2022-02-22 17:17:15 -0700

    improved scaling for tile labels on the board view
    I still don't love it, but it is better than it was. This is probably
    gonna get a touch hairy when we start animating these a bit.
    
    It might be nicer to have a way to estimate the text size during SSR.

---
+++ src/lib/scaledContent.ts
@@ -0,0 +1,20 @@
+export const scaledContent = (node) => {
+	const scaledContent = node.querySelector('.scaled-content');
+	scaledContent.style.transform = 'scale(1, 1)';
+
+	const { width: contentWidth, height: contentHeight } = scaledContent.getBoundingClientRect();
+	const { width: wrapperWidth, height: wrapperHeight } = node.getBoundingClientRect();
+
+	const scaleAmountX = Math.min((0.9 * wrapperWidth) / contentWidth, wrapperHeight / contentHeight);
+	const scaleAmountY = scaleAmountX;
+
+	if (scaleAmountX < 1) {
+		scaledContent.style.transform = `scale(${scaleAmountX}, ${scaleAmountY})`;
+	}
+
+	scaledContent.style.visibility = 'visible';
+
+	return {
+		destroy() {}
+	};
+};
--- src/routes/boards/[boardId].svelte
+++ src/routes/boards/[boardId].svelte
@@ -19,6 +19,7 @@
 </script>
 
 <script lang="ts">
+	import { scaledContent } from '$lib/scaledContent';
 	export let board: Board;
 
 	// TODO: size is computable from board.completions.length, but that mucks
@@ -28,10 +29,13 @@
 <div class="mt-2 grid gap-2 grid-cols-5 grid-rows-5 md:max-w-prose md:mx-auto">
 	{#each board.completions as tile}
 		<div
-			class="p-4 aspect-square border-2 border-info shadow-lg text-info-content rounded-lg flex justify-center items-center"
+			class="p-1 aspect-square border-2 border-info shadow-lg text-info-content rounded-lg flex justify-center items-center"
 			class:border-success={tile.state === 2}
+			use:scaledContent
 		>
-			<div class="text-center align-middle">{tile.objectives.label}</div>
+			<div class="scaled-content text-center invisible">
+				{tile.objectives.label}
+			</div>
 		</div>
 	{/each}
 </div>
