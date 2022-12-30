be70f16f2f7a0b20fe50fc13e3f195ed95fa1a7b
William Wall <william.a.wall.4@gmail.com>
2022-12-30 00:14:11 -0700

    was kinda hard to differentiate colors on mini board

--- src/lib/BoardMini.svelte
+++ src/lib/BoardMini.svelte
@@ -8,7 +8,7 @@
 	<div class="aspect-square grid gap-1 grid-cols-5 grid-rows-5">
 		{#each board.completions as tile}
 			<div
-				class="aspect-square w-3 h-3 bg-info shadow-lg text-info-content rounded"
+				class="aspect-square w-3 h-3 shadow-lg text-info-content rounded border-2 border-info"
 				class:bg-success={tile.state === 2}
 			/>
 		{/each}
